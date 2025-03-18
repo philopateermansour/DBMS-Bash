d563e15815e6f347b5c1a7bbadd2d45152e7f630
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-03-18 02:33:47 +0200

    feat: add some database functionalities

--- bin/dbms.sh
+++ bin/dbms.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+source ../src/database.sh
+source ../src/table.sh
+
+while true
+do
+    clear
+    echo -e "Welcome, $USER!\n\nMain Menu:"
+    PS3='Enter your choice number: '
+
+    select userInput in 'Create a database' 'List all databases' 'Connect to a database' 'Drop a database' 'Exit'
+    do
+        case $REPLY in
+        1) createDatabase; break;;
+        2) listDatabases; break;;
+        # 3) Handling; break;;
+        # 4) Handling; break;;
+        5) exit 0;;
+        *) echo 'Invalid option number, try again...';;
+        esac
+    done
+done
--- src/database.sh
+++ src/database.sh
@@ -1,1 +1,51 @@
 #! /bin/bash
+
+function createDatabase() {
+    clear
+    databasesPath='../databases'
+    read -p 'Enter database name: ' databaseName
+
+    isExists=`isDatabaseExists $databaseName`
+
+    if [[ $isExists == 1 ]]
+    then
+        echo 'Database already exists' >&2
+    else
+        mkdir "$databasesPath/$databaseName"
+
+        if [[ $? == 0 ]]
+        then
+            echo 'Database created successfully' >&1
+        else
+            echo 'An error occurred. Please try again later' >&2
+        fi
+    fi
+
+    read -p 'Press ENTER to return to the main menu'
+}
+
+function isDatabaseExists() {
+    databasesPath='../databases'
+
+    for currentDatabase in `ls $databasesPath`
+    do
+        if [[ -d $databasesPath/$currentDatabase && $currentDatabase == $1 ]]
+        then
+            echo 1
+            break
+        fi
+    done
+}
+
+function listDatabases() {
+    clear
+    databasesPath='../databases'
+
+    echo 'Your databases:'
+    for currentDatabase in `ls $databasesPath`
+    do
+        echo "- $currentDatabase" >&1
+    done
+
+    read -p 'Press ENTER to return to the main menu'
+}
