476dd520462ec5912ef48ab6b531986fc7b2e2bc
Philopater Mansour <philopater.mansour@gmail.com>
2025-03-19 03:51:32 +0200

    feat: Add support for foreign keys, auto-increment, and unique constraints

--- src/validation.sh
+++ src/validation.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+function isTableExists() {
+    fullPath=$DATABASES_PATH/$SELECTED_DATABASE
+    for currentTable in `ls $fullPath/`
+    do
+        if [[ -f "$fullPath/$currentTable" && $currentTable == $1.txt ]]
+        then
+            echo 1
+            break
+        fi
+    done
+}
 
 function validatePositiveInteger() {
 
@@ -22,7 +33,18 @@ function validateDataType() {
 }
 
 function validateName() {
-    if [[ "$1" =~ ^[a-zA-Z][a-zA-Z0-9_]+$ ]]
+    if [[ "$1" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+    then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+function validateForeignKey() {
+    
+    isExists=`isTableExists $1`
+    if [[ $isExists == 1 ]] && grep -q "$2" "$DATABASES_PATH/$SELECTED_DATABASE/.$1-md.txt"
     then
         echo 1
     else
