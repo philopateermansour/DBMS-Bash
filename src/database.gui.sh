f63f8b96954af4e5fcc0dba65c868cb326a5062a
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-03-27 20:38:01 +0200

    feat: add create database function

--- src/database.gui.sh
+++ src/database.gui.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
 source ../src/table.gui.sh
+
+function createDatabase() {
+    databaseName=`zenity --entry --title="Create a database" --text="Enter database name:"`
+
+    isValid=`validateName "$databaseName"`
+
+    if [[ $isValid == 0 ]]
+    then
+        zenity --error --text="Invalid name, name must start whith character and contains alphanumeric characters and underscores only"
+    else
+        isExists=`isDatabaseExists $databaseName`
+
+        if [[ $isExists == 1 ]]
+        then
+            zenity --error --text='Database already exists'
+        else
+            mkdir "$DATABASES_PATH/$databaseName"
+        fi
+    fi
+}
