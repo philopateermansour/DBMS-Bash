6e00c86487abe91b05f8d3641330f2b3ca169fc8
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-03-27 20:31:18 +0200

    feat: initialize database gui

--- bin/dbms.sh
+++ bin/dbms.sh
@@ -1,12 +1,47 @@
 #! /bin/bash
 
 shopt -s extglob
-
 source ../src/config.sh
-source ../src/database.sh
 
-while true
-do
+function main() {
+    if [[ $1 == "--gui" ]]
+    then
+        source ../src/database.gui.sh
+        
+        zenity --info --width=400 --text="This database management system provides essential tools for efficient data handling and organization.\
+        \n\nDeveloped by:\n- Philopateer Mansour\n- Muhamad Mamoun\n\nThis software is licensed under the MIT License." --title="Software License & Contributors"
+
+        while true; do graphicalMainMenu; done
+    elif [[ $1 == "--cli" ]]
+    then
+        source ../src/database.sh
+
+        echo -e "Software License & Contributors\n\nThis database management system provides essential tools for efficient data handling and organization.\
+        \n\nDeveloped by:\n- Philopateer Mansour\n- Muhamad Mamoun\n\nThis software is licensed under the MIT License."
+        read -p 'Press ENTER to continue...'
+
+        while true; do terminalMainMenu; done
+    else
+        echo "Invalid argument, use --gui or --cli only" >&2
+        exit 1
+    fi
+}
+
+function graphicalMainMenu() {
+    userInput=`zenity --list --width=500 --height=400\
+    --title="Main Menu" --text="Welcome, $USER!" --column="Options"\
+    "Create a database" "List all databases" "Connect to a database" "Drop a database" "Exit"`
+    
+    case $userInput in
+    "Create a database") createDatabase;;
+    "List all databases") listDatabases;;
+    "Connect to a database") connectToDatabase;;
+    "Drop a database") dropDatabase;;
+    @("Exit"|"")) echo "Good Bye, $USER."; exit 0;;
+    esac
+}
+
+function terminalMainMenu() {
     clear
     echo -e "Welcome, $USER!\n\nMain Menu:"
     PS3='Enter your choice number: '
@@ -22,4 +57,6 @@ do
         *) echo 'Invalid option number, try again...';;
         esac
     done
-done
+}
+
+main $1
---
+++ src/table.gui.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+source ../src/validation.sh
